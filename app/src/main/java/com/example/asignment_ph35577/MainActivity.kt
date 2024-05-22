648540fd6127642b85d13f19849fe505e4e53cbc
DoVietHung299 <doviethung2992004@gmail.com>
2024-05-22 15:24:22 +0700

    Initial commit

---
+++ app/src/main/java/com/example/asignment_ph35577/MainActivity.kt
@@ -0,0 +1,111 @@
+package com.example.asignment_ph35577
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.asignment_ph35577.ui.theme.Asignment_PH35577Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Asignment_PH35577Theme {
+                BoardingScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun BoardingScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.hinhnen),
+            contentDescription = "Background",
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.fillMaxSize()
+        )
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(16.dp)
+        ) {
+            Spacer(modifier = Modifier.padding(10.dp))
+
+            Text(
+                text = "MAKE YOUR",
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Gray,
+                textAlign = TextAlign.Left,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp)
+            )
+            Text(
+                text = "HOME BEAUTIFUL",
+                fontSize = 32.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Left,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(start = 10.dp)
+            )
+            Spacer(modifier = Modifier.height(26.dp))
+            Text(
+                text = "   The best simple place where you\n" +
+                        "        discover most wonderful furnitures\n" +
+                        "and make your home beautiful",
+                fontSize = 16.sp,
+                fontWeight = FontWeight.Normal,
+                color = Color.Gray,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.height(170.dp))
+            Button(
+                onClick = { /* TODO: Add action */ },
+                shape = MaterialTheme.shapes.medium,
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Black,
+                    contentColor = Color.White
+                ),
+                modifier = Modifier
+                    .padding(horizontal = 8.dp)
+            ) {
+                Text(text = "Get Started", fontSize = 20.sp, fontWeight = FontWeight.Bold)
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoardingScreenPreview() {
+    Asignment_PH35577Theme {
+        BoardingScreen()
+    }
+}
